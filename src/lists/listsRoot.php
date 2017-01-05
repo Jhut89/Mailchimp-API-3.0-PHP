0d6f5dda3cbfa5cac292fe8ddb22a8f84f2b3463
John Hutcheson <jhutcheosn@rsglab.com>
2017-01-05 16:37:45 -0500

    correcting indention

--- src/lists/listsRoot.php
+++ src/lists/listsRoot.php
@@ -1,196 +1,180 @@
 <?php
 
-class lists extends mailchimp {
+class lists extends mailchimp
+{
 
-  public $subclass_resource;
+    public $subclass_resource;
 
-  #SUBCLASS INSTANTIATIONS
-  public $webhooks;
-  public $signup_forms;
-  public $merge_fields;
-  public $growth_history;
-  public $clients;
-  public $activity;
-  public $abuse;
-  public $segments;
-  public $members;
-  public $interest_categories;
+    //SUBCLASS INSTANTIATIONS
+    public $webhooks;
+    public $signup_forms;
+    public $merge_fields;
+    public $growth_history;
+    public $clients;
+    public $activity;
+    public $abuse;
+    public $segments;
+    public $members;
+    public $interest_categories;
 
 
-  function __construct($apikey, $class_input)
-  {
-      parent::__construct($apikey);
-
-      if (isset($class_input))
-      {
-          $this->url .= '/lists/' . $class_input;;
-      } else
-      {
-          $this->url .= '/lists/';
-      }
-      
-      $this->subclass_resource = $class_input;
-  }
-
-	public function GET( $query_params = null )
-  {
-    $query_string = '';
-
-    if (is_array($query_params)) 
+    function __construct($apikey, $class_input)
     {
-        $query_string = $this->construct_query_params($query_params);
+        parent::__construct($apikey);
+
+        if (isset($class_input))
+        {
+            $this->url .= '/lists/' . $class_input;;
+        } else {
+            $this->url .= '/lists/';
+        }
+        $this->subclass_resource = $class_input;
+
     }
 
-    $url = $this->url . $query_string;
-    $response = $this->curl_get($url);
+    public function GET( $query_params = null )
+    {
+        $query_string = '';
 
-    return $response;
-  }
+        if (is_array($query_params))
+        {
+            $query_string = $this->construct_query_params($query_params);
+        }
 
-  public function BATCH_SUB($members = array(), $update_existing)
-  {
+        $url = $this->url . $query_string;
+        $response = $this->curl_get($url);
 
-    $params = array('members' => $members, 'update_existing' => $update_existing);
-    $payload = json_encode($params);
-
-    $url = $this->url;
-
-    $response = $this->curl_post($url, $payload);
-
-    return $response;
-
-  }
-
-  public function POST($name,
-                       $reminder,
-                       $emailtype, #bool
-                       $company,
-                       $address_street,
-                       $address_street2,
-                       $address_city,
-                       $address_state,
-                       $address_zip,
-                       $country,
-                       $from_name,
-                       $from_email,
-                       $subject,
-                       $language,
-                       $optional_parameters = array())
-  {
-
-    $params = array('name' => $name,
-        'permission_reminder' => $reminder,
-        'email_type_option' => $emailtype,
-
-        'contact' => array('company' => $company,
-            'address1' => $address_street,
-            'city' => $address_city,
-            'state' => $address_state,
-            'zip' => $address_zip,
-            'country' => $country),
-
-        'campaign_defaults' => array('from_name' => $from_name,
-            'from_email' => $from_email,
-            'subject' => $subject,
-            'language' => $language)
-    );
-
-    if (!is_null($address_street2)) {
-        $params['address2'] = $address_street2;
+        return $response;
     }
 
-    if (!is_null($optional_parameters['phone'])) {
-        $params['contact']['phone'] = $optional_parameters['phone'];
+    public function BATCH_SUB($members = array(), $update_existing)
+    {
+
+        $params = array('members' => $members, 'update_existing' => $update_existing);
+        $payload = json_encode($params);
+
+        $url = $this->url;
+
+        $response = $this->curl_post($url, $payload);
+
+        return $response;
+
     }
 
-    $payload = json_encode($params);
-    $url = $this->url;
+    public function POST($name, $reminder, $emailtype, $company, $address_street, $address_street2, $address_city, $address_state, $address_zip, $country, $from_name, $from_email, $subject, $language, $optional_parameters = array())
+    {
 
-    $response = $this->curl_post($url, $payload);
+        $params = array('name' => $name,
+                        'permission_reminder' => $reminder,
+                        'email_type_option' => $emailtype,
+                        'contact' => array('company' => $company,
+                                           'address1' => $address_street,
+                                           'city' => $address_city,
+                                           'state' => $address_state,
+                                           'zip' => $address_zip,
+                                           'country' => $country),
+                        'campaign_defaults' => array('from_name' => $from_name,
+                                                     'from_email' => $from_email,
+                                                     'subject' => $subject,
+                                                     'language' => $language)
+                        );
 
-    return $response;
+        if (!is_null($address_street2)) {
+            $params['address2'] = $address_street2;
+        }
 
-  }
+        if (!is_null($optional_parameters['phone'])) {
+            $params['contact']['phone'] = $optional_parameters['phone'];
+        }
 
-   public function PATCH($patch_params = null)
-  {
+        $payload = json_encode($params);
+        $url = $this->url;
 
-      $payload = json_encode($patch_params);
-      $url = $this->url;
+        $response = $this->curl_post($url, $payload);
 
-      $response = $this->curl_patch($url, $payload);
+        return $response;
 
-      return $response;
-  }
+    }
 
-  public function DELETE()
-  {
-      $url = $this->url;
-      $response = $this->curl_delete($url);
+    public function PATCH($patch_params = null)
+    {
 
-      return $response;
-  }
+        $payload = json_encode($patch_params);
+        $url = $this->url;
 
-  //SUBCLASS FUNCTIONS ------------------------------------------------------------
+        $response = $this->curl_patch($url, $payload);
 
-  public function webhooks( $class_input = null )
-  {
-    $this->webhooks = new lists_webhooks($this->apikey, $this->subclass_resource, $class_input);
-    return $this->webhooks;
-  }
+        return $response;
+    }
 
-  public function signup_forms( $class_input = null )
-  {
-    $this->signup_forms = new lists_signup_forms($this->apikey, $this->subclass_resource, $class_input);
-    return $this->signup_forms;
-  }
+    public function DELETE()
+    {
+        $url = $this->url;
+        $response = $this->curl_delete($url);
 
-  public function merge_fields( $class_input = null )
-  {
-    $this->merge_fields = new lists_merge_fields($this->apikey, $this->subclass_resource, $class_input);
-    return $this->merge_fields;
-  }
+        return $response;
+    }
 
-  public function growth_history( $class_input = null )
-  {
-    $this->growth_history = new lists_growth_history($this->apikey, $this->subclass_resource, $class_input);
-    return $this->growth_history;
-  }
+    //SUBCLASS FUNCTIONS ------------------------------------------------------------
 
-  public function clients( $class_input = null )
-  {
-    $this->clients = new lists_clients($this->apikey, $this->subclass_resource, $class_input);
-    return $this->clients;
-  }
+    public function webhooks( $class_input = null )
+    {
+        $this->webhooks = new lists_webhooks($this->apikey, $this->subclass_resource, $class_input);
+        return $this->webhooks;
+    }
 
-  public function activity( $class_input = null )
-  {
-    $this->activity = new lists_activity($this->apikey, $this->subclass_resource, $class_input);
-    return $this->activity;
-  } 
+    public function signup_forms( $class_input = null )
+    {
+        $this->signup_forms = new lists_signup_forms($this->apikey, $this->subclass_resource, $class_input);
+        return $this->signup_forms;
+    }
 
-  public function abuse_reports( $class_input = null )
-  {
-    $this->abuse = new lists_abuse_reports($this->apikey, $this->subclass_resource, $class_input);
-    return $this->abuse;
-  }
+    public function merge_fields( $class_input = null )
+    {
+        $this->merge_fields = new lists_merge_fields($this->apikey, $this->subclass_resource, $class_input);
+        return $this->merge_fields;
+    }
 
-  public function segments( $class_input = null )
-  {
-    $this->segments =  new lists_segments($this->apikey, $this->subclass_resource, $class_input);
-    return $this->segments;
-  }
+    public function growth_history( $class_input = null )
+    {
+        $this->growth_history = new lists_growth_history($this->apikey, $this->subclass_resource, $class_input);
+        return $this->growth_history;
+    }
 
-  public function members( $class_input = null )
-  {
-    $this->members = new lists_members($this->apikey, $this->subclass_resource, $class_input);
-    return $this->members;
-  }
+    public function clients( $class_input = null )
+    {
+        $this->clients = new lists_clients($this->apikey, $this->subclass_resource, $class_input);
+        return $this->clients;
+    }
 
-  public function interest_categories( $class_input = null )
-  {
-    $this->interest_categories = new lists_interest_categories($this->apikey, $this->subclass_resource, $class_input);
-    return $this->interest_categories;
-  }
+    public function activity( $class_input = null )
+    {
+        $this->activity = new lists_activity($this->apikey, $this->subclass_resource, $class_input);
+        return $this->activity;
+    }
+
+    public function abuse_reports( $class_input = null )
+    {
+        $this->abuse = new lists_abuse_reports($this->apikey, $this->subclass_resource, $class_input);
+        return $this->abuse;
+    }
+
+    public function segments( $class_input = null )
+    {
+        $this->segments =  new lists_segments($this->apikey, $this->subclass_resource, $class_input);
+        return $this->segments;
+    }
+
+    public function members( $class_input = null )
+    {
+        $this->members = new lists_members($this->apikey, $this->subclass_resource, $class_input);
+        return $this->members;
+    }
+
+    public function interest_categories( $class_input = null )
+    {
+        $this->interest_categories = new lists_interest_categories($this->apikey, $this->subclass_resource, $class_input);
+        return $this->interest_categories;
+    }
 
 }
