5f53ca42ff86acaa91c08c21b2b96f3a8d6f99fe
John Hutcheson <jhutcheosn@rsglab.com>
2017-01-19 15:44:57 -0500

    refactor templateFolders & templates

--- src/templateFolders/templateFoldersRoot.php
+++ src/templateFolders/templateFoldersRoot.php
@@ -2,6 +2,13 @@
 
 class Template_Folders extends Mailchimp
 {
+    //REQUIRED FIELDS DEFINITIONS
+    public $req_post_prarams = [
+        'name'
+    ];
+    public $req_patch_prarams = [
+        'name'
+    ];
 
     function __construct($apikey, $class_input)
     {
@@ -13,50 +20,4 @@ class Template_Folders extends Mailchimp
             $this->url .= '/template-folders/';
         }
     }
-
-    public function GET( $query_params = null )
-    {
-        $query_string = '';
-
-        if (is_array($query_params)) {
-            $query_string = $this->constructQueryParams($query_params);
-        }
-
-        $url = $this->url . $query_string;
-        $response = $this->curlGet($url);
-
-        return $response;
-    }
-
-    public function POST( $foldername )
-    {
-        $params = array('name' => $foldername);
-
-        $payload = json_encode($params);
-        $url = $this->url;
-        $response = $this->curlPost($url, $payload);
-
-        return $response;
-    }
-
-    public function PATCH( $foldername )
-    {
-        $params = array('name' => $foldername);
-
-        $payload = json_encode($params);
-        $url = $this->url;
-
-        $response = $this->curlPatch($url, $payload);
-
-        return $response;
-    }
-
-    public function DELETE()
-    {
-        $url = $this->url;
-        $response = $this->curlDelete($url);
-
-        return $response;
-    }
-
 }
--- src/templates/templatesRoot.php
+++ src/templates/templatesRoot.php
@@ -5,8 +5,17 @@ class Templates extends Mailchimp
 
     public $subclass_resource;
 
-    //SUBCLASS INSTANTIATIONS
+    //REQUIRED FIELDS DEFINITIONS
+    public $req_post_prarams = [
+        'name',
+        'html'
+    ];
+    public $req_patch_prarams = [
+        'name',
+        'html'
+    ];
 
+    //SUBCLASS INSTANTIATIONS
     public $default_content;
 
     function __construct($apikey, $class_input)
@@ -20,61 +29,6 @@ class Templates extends Mailchimp
         }
       
         $this->subclass_resource = $class_input;
-    }
-
-    public function GET( $query_params = null )
-    {
-        $query_string = '';
-
-        if (is_array($query_params)) {
-            $query_string = $this->constructQueryParams($query_params);
-        }
-
-        $url = $this->url . $query_string;
-        $response = $this->curlGet($url);
-
-        return $response;
-    }
-
-    public function POST( $name, $html , $folder_id = null )
-    {
-        $params = array('name' => $name, 'html' => $html);
-
-        if (!is_null($folder_id)) {
-            $params['folder_id'] = $folder_id;
-        }
-
-        $payload = json_encode($params);
-        $url = $this->url;
-
-        $response = $this->curlPost($url, $payload);
-
-        return $response;
-
-    }
-
-    public function PATCH( $name, $html, $folder_id = null )
-    {
-        $params = array('name' => $name, 'html' => $html);
-
-        if (!is_null($folder_id)) {
-            $params['folder_id'] = $folder_id;
-        }
-
-        $payload = json_encode($params);
-        $url = $this->url;
-
-        $response = $this->curlPatch($url, $payload);
-
-        return $response;
-    }
-
-    public function DELETE()
-    {
-        $url = $this->url;
-        $response = $this->curlDelete($url);
-
-        return $response;
     }
 
     //SUBCLASS FUNCTIONS ------------------------------------------------------------
