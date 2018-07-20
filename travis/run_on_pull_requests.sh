9eb5905c893d4638852c371852bb5f123c52be62
John Hutcheson <hutch@theresearchgroup.io>
2018-07-20 15:31:37 -0400

    testing linter

--- travis/run_on_pull_requests.sh
+++ travis/run_on_pull_requests.sh
@@ -13,41 +13,46 @@ ALTERED_FILES=$(git diff --name-only $TRAVIS_COMMIT_RANGE)
 
 echo "${ALTERED_FILES}"
 
-#for each changed file -> lint -> comment
-for changed_file in $ALTERED_FILES; do
-    # split file path into array
-    IFS='/' read -r -a split <<< "${changed_file}"
-    ROOT_DIR=${split[0]}
+# we only want to lint once per PR so lets do it for the 5.6 build
+if [ ${TRAVIS_PHP_VERSION} == "5.6" ]; then
 
-    if [ "${ROOT_DIR}" = "src" ]; then
-        # this variable is set and check to signal if lint errors are found
+    #for each changed file -> lint -> comment
+    for changed_file in $ALTERED_FILES; do
+        # split file path into array
+        IFS='/' read -r -a split <<< "${changed_file}"
+        ROOT_DIR=${split[0]}
 
-        echo -e "\n${changed_file} is in src directory\n"
+        if [ "${ROOT_DIR}" = "src" ]; then
+            # this variable is set and check to signal if lint errors are found
 
-        # file_report is a json representation of a phpcs report
-        json_file_report=$(./vendor/bin/phpcs --report=json --standard=PSR12 "${changed_file}")
+            echo -e "\n${changed_file} is in src directory\n"
 
-        #PHP script, comments on PR with lint issues, sets LINT_ERRORS="true" if errors are found
+            # file_report is a json representation of a phpcs report
+            json_file_report=$(./vendor/bin/phpcs --report=json --standard=PSR12 "${changed_file}")
 
-        LINT_ERRORS=$(php "$(pwd)/travis/CheckErrors.php" "${json_file_report}")
+            #PHP script, comments on PR with lint issues, sets LINT_ERRORS="true" if errors are found
 
-        if [ "${LINT_ERRORS}" = "true" ]; then
-            PR_PASSING="false"
+            LINT_ERRORS=$(php "$(pwd)/travis/CheckErrors.php" "${json_file_report}")
 
-            php "$(pwd)/travis/ReportError.php" "${changed_file}" "${json_file_report}"
+            if [ "${LINT_ERRORS}" = "true" ]; then
+                PR_PASSING="false"
 
-            #get a human readable report
-            file_report=$(./vendor/bin/phpcs --standard=PSR12 "${changed_file}")
+                php "$(pwd)/travis/ReportError.php" "${changed_file}" "${json_file_report}"
 
-            # Echo a report to standard out so that we can see it in build log
-            echo -e "\n\n${RED}${BOLD}${file_report}${NS}\n"
+                #get a human readable report
+                file_report=$(./vendor/bin/phpcs --standard=PSR12 "${changed_file}")
 
-            exit 1
+                # Echo a report to standard out so that we can see it in build log
+                echo -e "\n\n${RED}${BOLD}${file_report}${NS}\n"
+
+                exit 1
+            fi
+        else
+            echo -e "${GREEN}${changed_file} is ${BOLD}NOT${NS}${GREEN} in src directory and will not be linted${NS}\n"
         fi
-    else
-        echo -e "${GREEN}${changed_file} is ${BOLD}NOT${NS}${GREEN} in src directory and will not be linted${NS}\n"
-    fi
-done
+    done
+
+fi
 
 echo "${PR_PASSING}"
 
