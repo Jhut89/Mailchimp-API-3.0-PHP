e0a89f6ece11fc051bce604f4b2dabb4c9d23526
John Hutcheson <hutch@theresearchgroup.io>
2018-07-20 19:55:27 -0400

    remove un-needed func

--- src/Account.php
+++ src/Account.php
@@ -9,10 +9,6 @@ namespace MailchimpAPI;
  */
 class Account extends Mailchimp
 {
-    public function something()
-    {
-        print "this shouldnt be here!";
-    }
     /**
      * the url component for this endpoint
      */
