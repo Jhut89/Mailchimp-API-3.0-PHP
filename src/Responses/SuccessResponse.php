37cdd6819f6725741f71bb4e2386e52d22019e0f
John Hutcheson <jhutcheson@theresearchgrp.com>
2019-03-27 15:04:51 -0400

    fix callbacks, add tests

--- src/Responses/SuccessResponse.php
+++ src/Responses/SuccessResponse.php
@@ -18,15 +18,13 @@ class SuccessResponse extends MailchimpResponse
      * @param $body
      * @param $http_code
      * @param callable|null $success_callback
-     * @throws MailchimpException
      */
     public function __construct($headers, $body, $http_code, callable $success_callback = null)
     {
         parent::__construct($headers, $body, $http_code);
-        if (is_callable($success_callback)) {
-            call_user_func("foo");
-        } elseif ($success_callback && !is_callable($success_callback)) {
-            throw new MailchimpException("The success callback is not callable");
+
+        if ($success_callback) {
+            call_user_func($success_callback);
         }
     }
 }
